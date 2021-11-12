1d0ac350687d43f3e3b1836b056db9774a671290
Chen Wang <wangchen20@iscas.ac.cn>
2021-11-12 11:20:38 +0800

    added packages/services/Car

--- apply_patches.sh
+++ apply_patches.sh
@@ -9,10 +9,27 @@ TOP_PATCHES=$TOP/patches_aosp_riscv/aosp
 
 # Note: based off tag android-12.0.0_r3
 
+# frameworks/base
 PATH_REPO=frameworks/base
 PATH_DES_REPO=$TOP/$PATH_REPO
 PATH_SRC_REPO=$TOP_PATCHES/$PATH_REPO
 
-cp -f $PATH_SRC_REPO/packages/CtsShim/Android.bp $PATH_DES_REPO/packages/CtsShim/Android.bp
-cp -f -r $PATH_SRC_REPO/packages/CtsShim/apk/riscv64/ $PATH_DES_REPO/packages/CtsShim/apk
+cp -f $PATH_SRC_REPO/packages/CtsShim/Android.bp \
+      $PATH_DES_REPO/packages/CtsShim/Android.bp
+cp -f -r $PATH_SRC_REPO/packages/CtsShim/apk/riscv64/ \
+         $PATH_DES_REPO/packages/CtsShim/apk/
 
+# packages/services/Car
+PATH_REPO=packages/services/Car
+PATH_DES_REPO=$TOP/$PATH_REPO
+PATH_SRC_REPO=$TOP_PATCHES/$PATH_REPO
+
+cp -f $PATH_SRC_REPO/cpp/computepipe/example/Android.bp \
+      $PATH_DES_REPO/cpp/computepipe/example/Android.bp
+cp -f -r $PATH_SRC_REPO/cpp/computepipe/example/lib_riscv64/ \
+         $PATH_DES_REPO/cpp/computepipe/example/
+
+cp -f $PATH_SRC_REPO/cpp/computepipe/tests/runner/graph/stubgraph/Android.bp \
+      $PATH_DES_REPO/cpp/computepipe/tests/runner/graph/stubgraph/Android.bp
+cp -f -r $PATH_SRC_REPO/cpp/computepipe/tests/runner/graph/stubgraph/riscv64/ \
+         $PATH_DES_REPO/cpp/computepipe/tests/runner/graph/stubgraph/
